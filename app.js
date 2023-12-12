961d772dc4f556f7a02312e932bbac781db8be40
Alejandro Farias <151452578+Alejandro-FR@users.noreply.github.com>
2023-12-13 00:26:08 +0100

    volve

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function iniciar(){
             prin.style.marginTop='2%'; 
             prin.style.marginBottom='0%'; 
    
-    for (let i = 1; i <= 10; i++) {
-        for (let j = 1; j <= 20; j++) {
+    for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= 10; j++) {
             var ele = document.createElement('div');
             ele.id = 'caja' + i + j;
             prin.appendChild(ele);
@@ -19,10 +19,10 @@ function iniciar(){
             var rotada = false;
             cajon.addEventListener('click', function() {
             prin.style.marginTop='0%'; 
-            prin.style.marginLeft='5%';
+            prin.style.marginLeft='9%';
             prin.style.transitionDuration = '1s';
-                for (let i = 1; i <= 10; i++) {
-        for (let j = 1; j <= 20; j++) {
+                for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= 10; j++) {
             var cajon = document.getElementById('caja' + i + j);
         clickaqui(cajon);}}
     });
@@ -35,19 +35,19 @@ function clickaqui(cajaclick) {
 var sobre = cajaclick.getAttribute('data-sobre');
 if (sobre === 'false') {
 cajaclick.style.transform = 'rotate(45deg)';
-cajaclick.style.width = '50px';
-cajaclick.style.height = '50px';
+cajaclick.style.width = '100px';
+cajaclick.style.height = '100px';
 cajaclick.style.background = colorA();
-cajaclick.style.margin = '10px';
+cajaclick.style.margin = '21px';
 cajaclick.style.borderRadius = '0%';
 cajaclick.setAttribute('data-sobre', 'true');
 } else {
 cajaclick.style.transform = 'rotate(90deg)';
-cajaclick.style.width = '50px';
-cajaclick.style.height = '50px';
+cajaclick.style.width = '100px';
+cajaclick.style.height = '100px';
 cajaclick.style.background = colorA();
 cajaclick.style.borderRadius = '50%';
-cajaclick.style.margin = '10px';
+cajaclick.style.margin = '21px';
 cajaclick.setAttribute('data-sobre', 'false');
 }
 cajaclick.style.transitionDuration = '1s';
@@ -66,9 +66,9 @@ return color;
 
 function estilosP(elegante){
     elegante.style.border = 'solid 1px';
-    elegante.style.width = '30px';
-    elegante.style.height = '30px';
-    elegante.style.margin = '5px';
+    elegante.style.width = '60px';
+    elegante.style.height = '60px';
+    elegante.style.margin = '10px';
     elegante.style.float = 'left';
     elegante.style.boxSizing='border-box';
 }
